3ad0ea372d47be0a0b85299c275d7b4e511b8d39
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-08-26 11:56:29 +0500

    #NCB -1 Создание проекта

---
+++ ConsoleBackupData/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleBackupData
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
