1517d9a8a1386e031c4e2edb1da7db4c87745f8e
tpnotllnptl@gmail.com <tpnotllnptl@gmail.com>
2021-08-26 12:01:01 +0500

    #NCB -2 Создание интерфейсов (ICopyFailDirectory, IReadOptions)

---
+++ ConsoleBackupData/ICopyFailDirectory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBackupData
+{
+    interface ICopyFailDirectory
+    {
+        public Task<bool> CopyFailAsync();
+    }
+}
---
+++ ConsoleBackupData/IReadOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBackupData
+{
+    interface IReadOptions
+    {
+        public Task<bool> ReadOptionsAsync();
+    }
+}
